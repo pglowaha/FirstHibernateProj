4744a507e699c49cb679c1707d9a3bccd8cbdc06
Pawel <pglowaha@gmail.com>
2017-07-09 22:09:09 +0200

    After tut 12: Proxy Objects and Eager and Lazy Fetch Types

--- src/com/pglowaha/learning/hibernate/HibernateTest.java
+++ src/com/pglowaha/learning/hibernate/HibernateTest.java
@@ -35,6 +35,12 @@ public class HibernateTest {
 		session.getTransaction().commit();
 		session.close();
 		
+		user = null;
+		
+		session = sessionFactory.openSession();
+		user = (UserDetails) session.get(UserDetails.class, 1);
+		session.close();
+		System.out.println(user.getListOfAddresses().size());
 		sessionFactory.close();
 	}
 
