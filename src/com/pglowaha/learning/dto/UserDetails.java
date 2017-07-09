7e59c7532e616be10baab3fc7efc4ff5abd1ee9a
Pawel <pglowaha@gmail.com>
2017-07-09 16:55:27 +0200

    Add new fields and @Table annotation with name param

--- src/com/pglowaha/learning/dto/UserDetails.java
+++ src/com/pglowaha/learning/dto/UserDetails.java
@@ -1,23 +1,47 @@
 package com.pglowaha.learning.dto;
 
-import javax.persistence.Column;
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
-@Entity (name="USER_DETAILS")
+@Entity
+@Table (name="USER_DETAILS")
 public class UserDetails {
 	
+	@Id
 	private int userId;
 	private String userName;
-	@Id
-	@Column (name="USER_ID")
+	private Date joinedDate;
+	private String Address;
+	private String description;
+
+	
+	public Date getJoinedDate() {
+		return joinedDate;
+	}
+	public void setJoinedDate(Date joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 	public int getUserId() {
 		return userId;
 	}
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
-	@Column (name="USER_NAME")
 	public String getUserName() {
 		return userName + " from getter";
 	}
