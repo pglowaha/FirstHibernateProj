a21c56ed812da44b130c03f1e9fcfa1ce6373b3a
Pawel <pglowaha@gmail.com>
2017-07-09 20:09:13 +0200

    Add two embedded Address objects to UserDetails

--- src/com/pglowaha/learning/dto/UserDetails.java
+++ src/com/pglowaha/learning/dto/UserDetails.java
@@ -1,13 +1,14 @@
 package com.pglowaha.learning.dto;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
-import com.pglowaha.learning.Address;
 
 @Entity
 @Table (name="USER_DETAILS")
@@ -16,13 +17,28 @@ public class UserDetails {
 	@Id @GeneratedValue(strategy=GenerationType.AUTO)
 	private int userId;
 	private String userName;
-	private Address address;
 	@Embedded
-	public Address getAddress() {
-		return address;
+	@AttributeOverrides({
+		@AttributeOverride (name="street", column=@Column(name="HOME_STREET_NAME")),
+		@AttributeOverride (name="city", column=@Column(name="HOME_CITY_NAME")),
+		@AttributeOverride (name="state", column=@Column(name="HOME_STATE_NAME")),
+		@AttributeOverride (name="pincode", column=@Column(name="HOME_PIN_CODE")),
+	})
+	private Address homeAddress;
+	@Embedded
+	private Address officeAddress;
+	
+	public Address getHomeAddress() {
+		return homeAddress;
 	}
-	public void setAddress(Address address) {
-		this.address = address;
+	public void setHomeAddress(Address homeAddress) {
+		this.homeAddress = homeAddress;
+	}
+	public Address getOfficeAddress() {
+		return officeAddress;
+	}
+	public void setOfficeAddress(Address officeAddress) {
+		this.officeAddress = officeAddress;
 	}
 	public int getUserId() {
 		return userId;
